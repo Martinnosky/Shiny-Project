9709fa8808e11b7bdd7e23feb96b635bf222d217
Martinnosky <martinfornier1@gmail.com>
2026-03-13 11:08:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function sendMessage(){
 
-let input=document.getElementById("userInput").value;
+let input=document.getElementById("userInput").value.toLowerCase();
 
 let chat=document.getElementById("chat");
 
 chat.innerHTML += "<p>Utilisateur : "+input+"</p>";
 
-chat.innerHTML += "<p>Robot : Je ne comprends pas encore.</p>";
+let response="Je ne comprends pas.";
+
+if(input.includes("bonjour")){
+response="Bonjour humain, comment t'appelles-tu ?";
+}
+
+if(input.includes("je m'appelle")){
+response="Bonjour, ravi de te rencontrer.";
+}
+
+if(input.includes("jo")){
+response="Les prochains JO auront lieu à Paris en 2024.";
+}
+
+if(input.includes("au revoir")){
+response="Au revoir humain.";
+}
+
+chat.innerHTML += "<p>Robot : "+response+"</p>";
 
 document.getElementById("userInput").value="";
 }
