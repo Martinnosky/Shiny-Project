291ed054db2fc34976f550d542a29232feca18e6
Martinnosky <martinfornier1@gmail.com>
2026-03-13 11:06:08 +0100

    Update script.js
    base de js+

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function sendMessage(){
 
+let input=document.getElementById("userInput").value;
+
+let chat=document.getElementById("chat");
+
+chat.innerHTML += "<p>Utilisateur : "+input+"</p>";
+
+chat.innerHTML += "<p>Robot : Je ne comprends pas encore.</p>";
+
+document.getElementById("userInput").value="";
+}
